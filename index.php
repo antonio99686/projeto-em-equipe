097402f48a66ba1b02d467b1b953389dfb0c0dea
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-03-11 20:37:17 -0300

    index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/dashbord.css">
     <link rel="shortcut icon" href="img/logo.png">
-    <title>sistema</title>
+    <title>MERCADO ESTOQUE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
@@ -18,7 +18,7 @@ session_start();
 
 <nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
-<a class="navbar-brand"> estoque</a>
+<a class="navbar-brand"> ESTOQUE</a>
 <?php
 include("conexao.php");
 $sql = "SELECT * FROM produto";
