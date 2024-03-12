effc3038c46eb6710ab5a81646859a69ee5e69c2
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-03-11 21:26:41 -0300

    car

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/dashbord.css">
-    <link rel="shortcut icon" href="img/logo.png">
+    <link rel="shortcut icon" href="icon/car.png">
     <title>MERCADO ESTOQUE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -91,11 +91,7 @@ $dados = mysqli_fetch_assoc($resultado);
 
 </div>
 
-<div class="bruno">
 
-<h1> BRUNO GAYYY</h1>
-
-</div>
 </main>
 </body>
 </html>
