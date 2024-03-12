11ecacb9955d679cfdc459fccea06970ff479415
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-03-11 21:58:23 -0300

    js

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # git-git2024
-meu primeiro commit
-
-<h1> Informações </h1>
-Me chamo Antonio Mongelo. sou aluno do IFFAR Uruguaiana
-
-Segue no Insta @antonio_mongelo.thanks
+BRUNO E MONGELO TRABALHO EM DUPLAS
--- sql/produto.sql
+++ sql/produto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 11-Mar-2024 às 22:05
+-- Tempo de geração: 12-Mar-2024 às 00:55
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -29,12 +29,21 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `produto`;
 CREATE TABLE IF NOT EXISTS `produto` (
-  `id_produto` int NOT NULL,
+  `id_produto` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `quantidade` varchar(255) NOT NULL,
   `valor` varchar(255) NOT NULL,
   PRIMARY KEY (`id_produto`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id_produto`, `nome`, `quantidade`, `valor`) VALUES
+(6, 'FEIJAO', '1K', 'R$10,00'),
+(9, 'FEIJAO', '1K', 'R$ 8,99'),
+(8, 'ARROZ', '5K', 'R$10,00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
